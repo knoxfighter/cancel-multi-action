8bf158f2d515f48226ce3d33498d0d4c84df33d6
knoxfighter <knoxfighter@asdff45.de>
2021-01-30 05:25:59 +0100

    get all junks from the big http request

--- index.js
+++ index.js
@@ -9,69 +9,75 @@ const prNumber = regexResult[1];
 console.log(`Running on ${prNumber}`)
 
 if (prNumber === null) {
-  console.log("Let this workflow run.");
-  process.exit(0);
+    console.log("Let this workflow run.");
+    process.exit(0);
 }
 
 const httpPrOptions = {
-  hostname: 'api.github.com',
-  path: `/repos/${process.env.GITHUB_REPOSITORY}/pulls/${prNumber}`,
-  headers: {
-    'Content-Type': 'application/json',
-    'User-Agent': 'actions/cancel-multi-action'
-  },
-  method: 'GET',
-  json: true
+    hostname: 'api.github.com',
+    path: `/repos/${process.env.GITHUB_REPOSITORY}/pulls/${prNumber}`,
+    headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'actions/cancel-multi-action'
+    },
+    method: 'GET',
+    json: true
 }
 
 const prReq = https.request(httpPrOptions, (prRes) => {
-  prRes.on('data', (data) => {
-    const mergedData = Buffer.concat(data)
-    let parsed = JSON.parse(mergedData);
-    console.log(data);
-    if (prRes.statusCode != 200) {
-      console.log(`Error (${prRes.statusCode}): ${parsed.message}`);
-      process.exit(1);
-    } else if (parsed.head.label.startsWith('OpenFactorioServerManager')) {
-      const httpCancelOptions = {
-      hostname: 'api.github.com',
-        path: `/repos/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}/cancel`,
-        headers: {
-          'Authorization': `token ${core.getInput('token')}`,
-          'Content-Type': 'application/json',
-          'User-Agent': 'actions/cancel-action'
-        },
-        method: 'POST'
-      }
-    
-      const cancelReq = https.request(httpCancelOptions, (cancelRes) => {
-        cancelRes.on('data', (data) => {
-          if (cancelRes.statusCode != 202) {
-            let parsed = JSON.parse(data)
-            console.log(`Error (${cancelRes.statusCode}): ${parsed.message}`)
-            process.exit(1)
-          } else {
-            console.log('Cancelled successfully.')
-            process.exit(0)
-          }
-        })
-      })
+    let chunks = [];
 
-      cancelReq.on('error', (error) => {
-        console.log(`HTTP Error: ${error}`)
-        process.exit(1)
-      })
+    prRes.on('data', (data) => {
+        chunks.push(data);
+    });
 
-      cancelReq.end();
-    } else {
-      console.log('Let the tests run!');
-    }
-  })
+    prRes.on('end', () => {
+        let data = Buffer.concat(chunks);
+        let parsed = JSON.parse(data);
+
+        if (prRes.statusCode != 200) {
+            console.log(`Error (${prRes.statusCode}): ${parsed.message}`);
+            process.exit(1);
+        } else if (parsed.head.label.startsWith('OpenFactorioServerManager')) {
+            const httpCancelOptions = {
+                hostname: 'api.github.com',
+                path: `/repos/${process.env.GITHUB_REPOSITORY}/actions/runs/${process.env.GITHUB_RUN_ID}/cancel`,
+                headers: {
+                    'Authorization': `token ${core.getInput('token')}`,
+                    'Content-Type': 'application/json',
+                    'User-Agent': 'actions/cancel-action'
+                },
+                method: 'POST'
+            }
+
+            const cancelReq = https.request(httpCancelOptions, (cancelRes) => {
+                cancelRes.on('data', (data) => {
+                    if (cancelRes.statusCode != 202) {
+                        let parsed = JSON.parse(data)
+                        console.log(`Error (${cancelRes.statusCode}): ${parsed.message}`)
+                        process.exit(1)
+                    } else {
+                        console.log('Cancelled successfully.')
+                        process.exit(0)
+                    }
+                })
+            })
+
+            cancelReq.on('error', (error) => {
+                console.log(`HTTP Error: ${error}`)
+                process.exit(1)
+            })
+
+            cancelReq.end();
+        } else {
+            console.log('Let the tests run!');
+        }
+    })
 })
 
 prReq.on('error', (error) => {
-  console.log(`prHttp Error: ${error}`)
-  process.exit(1)
+    console.log(`prHttp Error: ${error}`)
+    process.exit(1)
 })
 
 prReq.end();
